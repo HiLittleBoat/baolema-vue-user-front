cd0e41694d60ce3e9f2846617ce8827a5ea4bf17
hxh <littleboat1231@163.com>
2023-03-23 16:20:47 +0800

    111

--- src/views/menu/Home.vue
+++ src/views/menu/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
 <!--轮播图-->
+    <div style="height: 300px">
+      <van-swipe :autoplay="3000">
+        <van-swipe-item v-for="(image, index) in images" :key="index">
+          <img :v-lazy="image" />
+        </van-swipe-item>
+      </van-swipe>
+    </div>
 <!--用户名/等级/去点餐-->
 <!--视频-->
 <!--日历-->
@@ -11,6 +18,8 @@
 
 </template>
 
+
+
 <script>
 import MenuFooter from "@/components/MenuFooter.vue";
 export default {
@@ -18,6 +27,14 @@ export default {
   components: {
     MenuFooter,
   },
+  data(){
+    return {
+      images: [
+        'https://img01.yzcdn.cn/vant/apple-1.jpg',
+        'https://img01.yzcdn.cn/vant/apple-2.jpg',
+      ],
+    };
+  }
 }
 </script>
 
